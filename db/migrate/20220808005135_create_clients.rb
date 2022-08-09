a903cad8499a05b458cab8e3ad58bf2bedc7ab88
iprogrammist <110420915+iprogrammist@users.noreply.github.com>
2022-08-09 03:00:51 +0300

    Add Barbers migration

--- db/migrate/20220808005135_create_clients.rb
+++ db/migrate/20220808005135_create_clients.rb
@@ -1,11 +1,13 @@
 class CreateClients < ActiveRecord::Migration[7.0]
   def change
-    create_table :client do |t|
+    create_table :clients do |t|
       t.text :name
       t.text :phone
       t.text :datestamp
       t.text :barber
       t.text :color
+
+      t.timestamps
     end
 
   end
